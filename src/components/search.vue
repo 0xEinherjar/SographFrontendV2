c626389de163058b7ff1e1a5521ab0ecfeff9937
Einherjar <rodrigo.silva1186@etec.sp.gov.br>
2024-07-01 02:02:24 -0300

    add search function

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,14 +1,26 @@
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const formSearch = ref("");
+
+function search() {
+  if (!formSearch.value) return;
+  router.push({ path: `/user/${formSearch.value.toLowerCase()}` });
+  formSearch.value = "";
+}
+</script>
 <!-- prettier-ignore -->
 <template>
-  <form class="search">
+  <form class="search" @submit.prevent="search">
     <button class="search__button">
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path opacity="0.4" d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" fill="#9AA0A6"/>
         <path d="M21.2996 22.0001C21.1196 22.0001 20.9396 21.9301 20.8096 21.8001L18.9496 19.9401C18.6796 19.6701 18.6796 19.2301 18.9496 18.9501C19.2196 18.6801 19.6596 18.6801 19.9396 18.9501L21.7996 20.8101C22.0696 21.0801 22.0696 21.5201 21.7996 21.8001C21.6596 21.9301 21.4796 22.0001 21.2996 22.0001Z" fill="#9AA0A6"/>
       </svg>
     </button>
-    <input class="search__input" type="text" placeholder="Search">
+    <input class="search__input" type="text" v-model="formSearch" placeholder="Search">
     <span class="search__badge"><span>s</span></span>
   </form>
 </template>
